b7bf5a7c5c4af30aa9e7d4fa34154b2482f9ae06
Sahith M <sahith007@gmail.com>
2021-10-28 01:49:00 +0530

    First commit
    Signed-off-by: sahith <sahith007@gmail.com>

---
+++ src/postgres/creds.java
@@ -0,0 +1,7 @@
+package postgres;
+
+class creds {
+    public static final String url = "jdbc:postgresql://john.db.elephantsql.com/rnypvzky";
+    public static final String username = "rnypvzky";
+    public static final String password = "L_fJIvDZd2K-e1dxrC0sFbMEDezt87GA";
+}
