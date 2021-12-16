23cf3f63c165c92b86f6e370a36fffee04a7a7c2
R.Srikesh <amenu4cse20354@am.students.amrita.edu>
2021-12-16 23:02:25 +0530

    Album ID to Artist ID

---
+++ src/postgres/AlbumIDtoArtistID.java
@@ -0,0 +1,24 @@
+package postgres;
+import java.sql.*;
+
+public class AlbumIDtoArtistID {
+    public static String main(String Album_ID){
+        Connection c;
+        Statement stmt;
+        String Artist_ID = "";
+        try{
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(creds.url, creds.username, creds.password);
+            stmt = c.createStatement();
+            String sql = "SELECT artist_id from albums where album_id = '"+Album_ID+"';";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                Artist_ID = rs.getString("artist_id");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return Artist_ID;
+    }
+}
